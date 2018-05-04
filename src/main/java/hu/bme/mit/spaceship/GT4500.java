5ef9613c8102a503c8a1802a13406e8fb78e8102
meres <frigh.adam@hotmail.com>
2018-05-04 00:45:17 -0700

    fix

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -51,7 +51,7 @@ public class GT4500 implements SpaceShip {
               firingSuccess = primaryTorpedoStore.fire(1);
               wasPrimaryFiredLast = true;
             }
-
+//komment
             // if both of the stores are empty, nothing can be done, return failure
           }
         }
